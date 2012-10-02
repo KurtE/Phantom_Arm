dfcaa95f3aa05aa2ea16360bcb8ddf6275c9c94b
Kurt Eckhardt <kurte@rockisland.com>
2012-10-02 10:29:07 -0700

    VB Debug app added
    Decided to also include my VB application I use to forward messages from
    Commander to Robot.  The way I used this is to move the XBee from the
    robot to USB one connected to PC, Connected the FTDI cable to arbotix
    board.  Then in this VB app selected the comm ports and did a connect...

---
+++ VB Test App/XBEE PC DIY Remote/Form1.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.ComGB = New System.Windows.Forms.GroupBox()
+        Me.ComLB = New System.Windows.Forms.ComboBox()
+        Me.Connect = New System.Windows.Forms.Button()
+        Me.CommThread = New System.ComponentModel.BackgroundWorker()
+        Me.LCDLB = New System.Windows.Forms.ListBox()
+        Me.LCDGroup = New System.Windows.Forms.GroupBox()
+        Me.PMTerminal = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.LBTerminalCount = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LBTerminalSelectAll = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LBTerminalCopytoClipBoard = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LBTerminalClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LCDLBClear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.XBeeDLContextMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.XBDLCM_DeleteItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.XBDLCM_Clear = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.RoboCommGB = New System.Windows.Forms.GroupBox()
+        Me.RobotComLB = New System.Windows.Forms.ComboBox()
+        Me.ComGB.SuspendLayout()
+        Me.LCDGroup.SuspendLayout()
+        Me.PMTerminal.SuspendLayout()
+        Me.XBeeDLContextMenu.SuspendLayout()
+        Me.RoboCommGB.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComGB
+        '
+        Me.ComGB.Controls.Add(Me.ComLB)
+        Me.ComGB.Location = New System.Drawing.Point(12, 12)
+        Me.ComGB.Name = "ComGB"
+        Me.ComGB.Size = New System.Drawing.Size(136, 47)
+        Me.ComGB.TabIndex = 12
+        Me.ComGB.TabStop = False
+        Me.ComGB.Text = "Commander Comm Port"
+        '
+        'ComLB
+        '
+        Me.ComLB.FormattingEnabled = True
+        Me.ComLB.Location = New System.Drawing.Point(1, 19)
+        Me.ComLB.Name = "ComLB"
+        Me.ComLB.Size = New System.Drawing.Size(123, 21)
+        Me.ComLB.TabIndex = 1
+        '
+        'Connect
+        '
+        Me.Connect.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Connect.Location = New System.Drawing.Point(376, 29)
+        Me.Connect.Name = "Connect"
+        Me.Connect.Size = New System.Drawing.Size(97, 23)
+        Me.Connect.TabIndex = 15
+        Me.Connect.Text = "Connect"
+        Me.Connect.UseVisualStyleBackColor = True
+        '
+        'CommThread
+        '
+        Me.CommThread.WorkerReportsProgress = True
+        Me.CommThread.WorkerSupportsCancellation = True
+        '
+        'LCDLB
+        '
+        Me.LCDLB.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.LCDLB.FormattingEnabled = True
+        Me.LCDLB.Location = New System.Drawing.Point(3, 16)
+        Me.LCDLB.Name = "LCDLB"
+        Me.LCDLB.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.LCDLB.Size = New System.Drawing.Size(455, 167)
+        Me.LCDLB.TabIndex = 0
+        '
+        'LCDGroup
+        '
+        Me.LCDGroup.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LCDGroup.Controls.Add(Me.LCDLB)
+        Me.LCDGroup.Location = New System.Drawing.Point(15, 64)
+        Me.LCDGroup.Name = "LCDGroup"
+        Me.LCDGroup.Size = New System.Drawing.Size(461, 186)
+        Me.LCDGroup.TabIndex = 43
+        Me.LCDGroup.TabStop = False
+        Me.LCDGroup.Text = "LCD"
+        '
+        'PMTerminal
+        '
+        Me.PMTerminal.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LBTerminalCount, Me.LBTerminalSelectAll, Me.LBTerminalCopytoClipBoard, Me.LBTerminalClear})
+        Me.PMTerminal.Name = "PMTerminal"
+        Me.PMTerminal.Size = New System.Drawing.Size(123, 92)
+        '
+        'LBTerminalCount
+        '
+        Me.LBTerminalCount.Name = "LBTerminalCount"
+        Me.LBTerminalCount.Size = New System.Drawing.Size(122, 22)
+        Me.LBTerminalCount.Text = "Count..."
+        '
+        'LBTerminalSelectAll
+        '
+        Me.LBTerminalSelectAll.Name = "LBTerminalSelectAll"
+        Me.LBTerminalSelectAll.Size = New System.Drawing.Size(122, 22)
+        Me.LBTerminalSelectAll.Text = "Select All"
+        '
+        'LBTerminalCopytoClipBoard
+        '
+        Me.LBTerminalCopytoClipBoard.Name = "LBTerminalCopytoClipBoard"
+        Me.LBTerminalCopytoClipBoard.Size = New System.Drawing.Size(122, 22)
+        Me.LBTerminalCopytoClipBoard.Text = "Copy"
+        '
+        'LBTerminalClear
+        '
+        Me.LBTerminalClear.Name = "LBTerminalClear"
+        Me.LBTerminalClear.Size = New System.Drawing.Size(122, 22)
+        Me.LBTerminalClear.Text = "Clear"
+        '
+        'LCDLBClear
+        '
+        Me.LCDLBClear.Name = "LCDLBClear"
+        Me.LCDLBClear.Size = New System.Drawing.Size(152, 22)
+        Me.LCDLBClear.Text = "Clear"
+        '
+        'XBeeDLContextMenu
+        '
+        Me.XBeeDLContextMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.XBDLCM_DeleteItem, Me.XBDLCM_Clear})
+        Me.XBeeDLContextMenu.Name = "PMTerminal"
+        Me.XBeeDLContextMenu.Size = New System.Drawing.Size(135, 48)
+        '
+        'XBDLCM_DeleteItem
+        '
+        Me.XBDLCM_DeleteItem.Name = "XBDLCM_DeleteItem"
+        Me.XBDLCM_DeleteItem.Size = New System.Drawing.Size(134, 22)
+        Me.XBDLCM_DeleteItem.Text = "Delete Item"
+        '
+        'XBDLCM_Clear
+        '
+        Me.XBDLCM_Clear.Name = "XBDLCM_Clear"
+        Me.XBDLCM_Clear.Size = New System.Drawing.Size(134, 22)
+        Me.XBDLCM_Clear.Text = "Clear List"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 250
+        '
+        'RoboCommGB
+        '
+        Me.RoboCommGB.Controls.Add(Me.RobotComLB)
+        Me.RoboCommGB.Location = New System.Drawing.Point(177, 12)
+        Me.RoboCommGB.Name = "RoboCommGB"
+        Me.RoboCommGB.Size = New System.Drawing.Size(122, 47)
+        Me.RoboCommGB.TabIndex = 50
+        Me.RoboCommGB.TabStop = False
+        Me.RoboCommGB.Text = "Robot Comm Port"
+        '
+        'RobotComLB
+        '
+        Me.RobotComLB.FormattingEnabled = True
+        Me.RobotComLB.Location = New System.Drawing.Point(1, 19)
+        Me.RobotComLB.Name = "RobotComLB"
+        Me.RobotComLB.Size = New System.Drawing.Size(122, 21)
+        Me.RobotComLB.TabIndex = 1
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(484, 262)
+        Me.Controls.Add(Me.RoboCommGB)
+        Me.Controls.Add(Me.Connect)
+        Me.Controls.Add(Me.ComGB)
+        Me.Controls.Add(Me.LCDGroup)
+        Me.MinimumSize = New System.Drawing.Size(500, 300)
+        Me.Name = "Form1"
+        Me.Text = "Commander Debug Terminal"
+        Me.ComGB.ResumeLayout(False)
+        Me.LCDGroup.ResumeLayout(False)
+        Me.PMTerminal.ResumeLayout(False)
+        Me.XBeeDLContextMenu.ResumeLayout(False)
+        Me.RoboCommGB.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ComGB As System.Windows.Forms.GroupBox
+    Friend WithEvents ComLB As System.Windows.Forms.ComboBox
+    Friend WithEvents Connect As System.Windows.Forms.Button
+    Friend WithEvents CommThread As System.ComponentModel.BackgroundWorker
+    Friend WithEvents LCDLB As System.Windows.Forms.ListBox
+    Friend WithEvents LCDGroup As System.Windows.Forms.GroupBox
+    Friend WithEvents PMTerminal As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents LCDLBClear As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LBTerminalSelectAll As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LBTerminalCopytoClipBoard As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LBTerminalClear As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents XBeeDLContextMenu As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents XBDLCM_DeleteItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents XBDLCM_Clear As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents LBTerminalCount As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RoboCommGB As System.Windows.Forms.GroupBox
+    Friend WithEvents RobotComLB As System.Windows.Forms.ComboBox
+
+End Class
---
+++ VB Test App/XBEE PC DIY Remote/Form1.vb
@@ -0,0 +1,347 @@
+﻿Imports System.Text.Encoding 'From VB Pipe
+Public Class Form1
+    ' This delegate enables asynchronous calls for setting
+    ' the text property on a TextBox control.
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+    Private Shared mut As New Mutex()
+
+    Public SCListBox As New StringCollection        ' use this to communicate between background thread and main thread...
+
+    ' Lets define a bunch of global junk, later should clean up!
+    Public fDataHasChanged As Boolean        '
+    Public XBSW As Stopwatch
+    Public MaxListCount As Integer
+    Public XBeeCommport As String
+    Public RobotCommport As String
+
+
+
+
+    Private Function intToByte(ByVal i As Integer)
+
+        Dim bArray As Byte() = {i And 255, (i >> 8) And 255, (i >> 16) And 255, (i >> 24) And 255}
+        intToByte = bArray
+
+    End Function
+
+    Private Sub Form1_FormClosed(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        My.Settings.MainFormSize = Size
+
+    End Sub
+    '========= VB Pipe end==============
+
+
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        ' Show all available COM ports.
+        Size = My.Settings.MainFormSize   ' see if we can save and restore the size
+
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComLB.Items.Add(sp)
+            RobotComLB.Items.Add(sp)
+        Next
+
+        ComLB.SelectedIndex = ComLB.FindString(My.Settings.Commport)
+        RobotComLB.SelectedIndex = RobotComLB.FindString(My.Settings.RobotCommPort)
+
+        If ComLB.SelectedIndex = -1 Then
+            Connect.Enabled = False
+
+        End If
+
+        MaxListCount = My.Settings.MaxListCount
+
+
+        ' Lets try to reload the collection of destinations into the list
+
+        ' Initialize the state of our cached buttons.
+        fDataHasChanged = False
+        ' create our timer
+        XBSW = New Stopwatch()
+    End Sub
+
+
+
+    Private Sub Connect_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Connect.Click
+        If Connect.Text = "Connect" Then
+            Timer1.Enabled = True       ' also get our timer running...
+            ' Connect.Text = "Disconnect"
+            XBeeCommport = ComLB.Items(ComLB.SelectedIndex)
+            RobotCommport = RobotComLB.Items(RobotComLB.SelectedIndex)
+
+            CommThread.RunWorkerAsync()
+        Else
+            '            Connect.Text = "Connect"
+            CommThread.CancelAsync()            ' turn off the thread...
+        End If
+
+    End Sub
+
+
+    Public Sub ClearInputBuffer(ByRef com1 As IO.Ports.SerialPort)
+        Dim cbRead As Byte
+        Dim b(1) As Byte
+
+        com1.DiscardInBuffer()    ' flush out anything we have
+        cbRead = 1
+        com1.ReadTimeout = 100 'wait a maximum of .1 seconds for a response
+        Try
+            While cbRead > 0
+                cbRead = com1.Read(b, 0, 1)
+            End While
+
+        Catch e As TimeoutException
+            'Debug.Print("TO: Clear Input Buffer")
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+
+    '==============================================================================
+    ' [DisplayRemoteString (pStr, cbOffset, cbStr)]
+    ' 
+    ' This function takes care of displaying string and or a number sent to us 
+    ' rom the remote robot.  For now hard coded to a specific location on line 2...
+    '
+    ' This function will also switch to the appropriate display mode if necessary.
+    '==============================================================================
+    Public Sub DisplayRemoteString(ByRef pstr() As Byte, ByVal cbOffset As Byte, ByVal cbStr As Byte)
+        ' Make sure we are in the right mode to display the data
+
+        If cbStr Then
+            Dim s As String = Mid(System.Text.Encoding.ASCII.GetString(pstr), cbOffset + 1, cbStr)
+
+            SyncLock SCListBox.SyncRoot
+                SCListBox.Add(s)
+            End SyncLock
+
+        End If
+        Return
+    End Sub
+
+
+    Private Sub CommThread_DoWork(ByVal sender As System.Object, ByVal e As System.ComponentModel.DoWorkEventArgs) Handles CommThread.DoWork
+
+        CommThread.ReportProgress(0)
+        XBSW.Start()
+        Dim ab(120) As Byte
+        Dim abCommander(4096) As Byte
+        Dim cbCommander As UInt16
+        Dim cb As Byte
+        Dim fDidSomething As Boolean
+        'Dim Counter As Byte
+
+        Try
+
+            Using com1 As IO.Ports.SerialPort = _
+                My.Computer.Ports.OpenSerialPort(XBeeCommport, 38400, IO.Ports.Parity.None)
+                com1.Handshake = IO.Ports.Handshake.None
+                com1.DtrEnable = False
+                com1.NewLine = Chr(13)
+
+                Using com2 As IO.Ports.SerialPort = _
+                    My.Computer.Ports.OpenSerialPort(RobotCommport, 38400, IO.Ports.Parity.None)
+                    com2.Handshake = IO.Ports.Handshake.None
+                    com2.DtrEnable = False
+                    com2.NewLine = Chr(13)
+
+                    cb = 0
+                    While Not CommThread.CancellationPending              ' turn off the thread...
+                        fDidSomething = False
+                        ' Echo any characters we receive on Comm 1 to Comm 2
+                        cbCommander = com1.BytesToRead
+                        If (cbCommander) Then
+                            com1.Read(abCommander, 0, cbCommander)
+                            com2.Write(abCommander, 0, cbCommander)
+                            com1.BaseStream.Flush()
+                            fDidSomething = True
+                        End If
+
+                        ' and echo anything robot sends us to our listbox...
+                        While (com2.BytesToRead)
+                            fDidSomething = True
+                            ab(cb) = com2.ReadByte()
+
+                            If ab(cb) = 13 Then
+                                DisplayRemoteString(ab, 0, cb)
+                                cb = 0
+                            ElseIf ab(cb) >= 32 Then
+                                cb = cb + 1
+                                If (cb >= 120) Then
+                                    DisplayRemoteString(ab, 0, cb)
+                                    cb = 0
+                                End If
+                            End If
+                            If Not fDidSomething Then
+                                System.Threading.Thread.Sleep(2)  ' try to completely hog the system!
+                            End If
+
+                        End While
+
+                    End While
+
+                End Using
+
+            End Using
+
+        Catch ex As Exception
+
+        End Try
+
+        CommThread.ReportProgress(99)
+
+    End Sub
+
+
+    Private Sub CommThread_ProgressChanged(ByVal sender As Object, ByVal e As System.ComponentModel.ProgressChangedEventArgs) Handles CommThread.ProgressChanged
+        If e.ProgressPercentage < 10 Then
+            LCDLB.Items.Add("*** Thread Start ***")
+            LCDLB.TopIndex = LCDLB.Items.Count - 1
+            Connect.Text = "Disconnect"
+        ElseIf e.ProgressPercentage > 90 Then
+            LCDLB.Items.Add("*** Thread Canceled ***")
+            LCDLB.TopIndex = LCDLB.Items.Count - 1
+            Timer1.Enabled = False  'We can stop our timer now...
+            Connect.Text = "Connect"
+
+            Try
+
+                Using com1 As IO.Ports.SerialPort = _
+                        My.Computer.Ports.OpenSerialPort(XBeeCommport, 38400, IO.Ports.Parity.None)
+                    com1.Handshake = IO.Ports.Handshake.None
+                    System.Threading.Thread.Sleep(500)
+                    ClearInputBuffer(com1)      ' make sure there is nothing there to start with...
+
+                    ClearInputBuffer(com1)      ' make sure there is nothing there to start with...
+
+                End Using
+            Catch ex As Exception
+
+            End Try
+        End If
+    End Sub
+
+
+    Private Sub ComLB_DropDown(sender As Object, e As System.EventArgs) Handles ComLB.DropDown
+        ComLB.Items.Clear() ' first clear our list.
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            ComLB.Items.Add(sp)
+        Next
+
+        ComLB.SelectedIndex = ComLB.FindString(My.Settings.Commport)
+
+        Connect.Enabled = False
+    End Sub
+    Private Sub RobotComLB_DropDown(sender As Object, e As System.EventArgs) Handles RobotComLB.DropDown
+
+        RobotComLB.Items.Clear() ' first clear our list.
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            RobotComLB.Items.Add(sp)
+        Next
+
+        RobotComLB.SelectedIndex = RobotComLB.FindString(My.Settings.RobotCommPort)
+
+        Connect.Enabled = False
+    End Sub
+
+
+    Private Sub ComLB_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComLB.SelectedIndexChanged
+        ' Show all available COM ports.
+
+        If (ComLB.SelectedIndex >= 0) Then
+            My.Settings.Commport = ComLB.Items(ComLB.SelectedIndex).ToString
+            Connect.Enabled = True
+        Else
+            ' nothing selected, disable a few controls.
+            Connect.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub RobotComLB_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles RobotComLB.SelectedIndexChanged
+        ' Show all available COM ports.
+
+        If (RobotComLB.SelectedIndex >= 0) Then
+            My.Settings.RobotCommPort = RobotComLB.Items(RobotComLB.SelectedIndex).ToString
+            Connect.Enabled = True
+        Else
+            ' nothing selected, disable a few controls.
+            Connect.Enabled = False
+        End If
+
+    End Sub
+    Private Sub LCDLB_MouseDown1(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles LCDLB.MouseDown
+        If e.Button = Windows.Forms.MouseButtons.Right Then
+            LCDLB.ContextMenuStrip = PMTerminal
+        End If
+
+    End Sub
+
+    Private Sub LCDLBClear_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles LBTerminalClear.Click
+        LCDLB.Items.Clear() ' clear out the listbox
+    End Sub
+
+    Private Sub LBTerminalSelectAll_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LBTerminalSelectAll.Click
+        Dim i As Integer
+        For i = 0 To LCDLB.Items.Count - 1
+            LCDLB.SetSelected(i, True)
+        Next
+
+    End Sub
+
+    Private Sub LBTerminalCopytoClipBoard_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles LBTerminalCopytoClipBoard.Click
+        Dim i As Integer
+        Dim s As String = ""
+        For i = 0 To LCDLB.Items.Count - 1
+            If LCDLB.GetSelected(0) Then
+                s = s + LCDLB.Items.Item(i).ToString + ControlChars.CrLf
+            End If
+        Next
+        If s <> "" Then
+            Clipboard.SetDataObject(s)
+        End If
+
+    End Sub
+
+    Private Sub LBTerminalCount_Click(sender As Object, e As EventArgs) Handles LBTerminalCount.Click
+        Dim NewCountString As String
+        NewCountString = InputBox("Enter Terminal List Max Count: ", "XBee DIY Remote", MaxListCount.ToString)
+        Try
+            MaxListCount = Integer.Parse(NewCountString, Globalization.NumberStyles.Integer)
+            My.Settings.MaxListCount = MaxListCount     ' save away the maximum count
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+        Dim NewObj As Object
+        If SCListBox.Count > 0 Then
+            LCDLB.BeginUpdate()
+            While SCListBox.Count > 0
+                SyncLock SCListBox.SyncRoot                 ' Minimize the time we hold the semaphore to simply remove an item...
+                    NewObj = SCListBox(0)                   'get items from the start and add to our listbox
+                    SCListBox.RemoveAt(0)                   'Delete that item from our list
+                End SyncLock
+                LCDLB.Items.Add(NewObj)                     'get items from the start and add to our listbox
+                If LCDLB.Items.Count > MaxListCount Then
+                    LCDLB.Items.RemoveAt(0)                 ' Getting to large so lets remove the first item...
+                End If
+            End While
+            ' Duplicate loop to remove excess items... Will take care of cases where stuff was added elsewhere
+            While LCDLB.Items.Count > MaxListCount
+                LCDLB.Items.RemoveAt(0)                 ' Getting to large so lets remove the first item...
+            End While
+            LCDLB.TopIndex = LCDLB.Items.Count - 1  ' And Scroll to that location...
+            LCDLB.EndUpdate()
+        End If
+    End Sub
+
+  
+End Class
+
+
